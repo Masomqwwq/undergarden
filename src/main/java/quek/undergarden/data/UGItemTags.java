8cac6187256809ee389bfc6d2c2a7911f90e9526
Masomqwwq <masomqwwq@gmail.com>
2024-12-09 19:42:16 -0600

    Update UGItemTags.java

--- src/main/java/quek/undergarden/data/UGItemTags.java
+++ src/main/java/quek/undergarden/data/UGItemTags.java
@@ -3,7 +3,6 @@ package quek.undergarden.data;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.data.tags.TagsProvider.TagLookup;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.Tags;
